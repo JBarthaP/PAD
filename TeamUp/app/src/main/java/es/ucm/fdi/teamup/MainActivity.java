aff6089b93ea7fd009667ac5f29eea29978adaa9
Rudaco2001 <73298874+Rudaco2001@users.noreply.github.com>
2023-03-16 14:19:38 +0100

    empezado primeras vistas

--- TeamUp/app/src/main/java/es/ucm/fdi/teamup/MainActivity.java
+++ TeamUp/app/src/main/java/es/ucm/fdi/teamup/MainActivity.java
@@ -2,13 +2,25 @@ package es.ucm.fdi.teamup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button createTeamsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createTeamsButton = findViewById(R.id.createTeamButton);
+        createTeamsButton.setOnClickListener((view)->{
+            Intent intent = new Intent(this, TeamsActivity.class);
+            startActivity(intent);
+        });
+
+
     }
+
+
 }
---
+++ TeamUp/app/src/main/java/es/ucm/fdi/teamup/TeamsActivity.java
@@ -0,0 +1,14 @@
+package es.ucm.fdi.teamup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teams);
+    }
+}
